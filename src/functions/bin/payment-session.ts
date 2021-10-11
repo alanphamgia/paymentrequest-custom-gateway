595971f8d1a7e9e9b72bf21792c9b59bda0ed9f6
dev.alanphamgiadigital <dev.phamgiadigital@gmail.com>
2021-10-12 03:02:48 +1030

    change

--- src/functions/bin/payment-session.ts
+++ src/functions/bin/payment-session.ts
@@ -1,13 +1,19 @@
 import axios from 'axios'
+import { APIGatewayEvent, Context, Callback } from "aws-lambda";
 
+exports.handler = async function ( event: APIGatewayEvent, context: Context, callback: Callback)  {
 
-exports.handler = async function (req, res) {
+  const requestBody = JSON.parse(event.body);
+  console.log(event);
   try {
-    const {publicToken} = req.query
+    const {publicToken} = requestBody.query;
     const resp = await axios.get(`https://payment.snipcart.com/api/public/custom-payment-gateway/payment-session?publicToken=${publicToken}`)
-    return res.json(resp.data)
+    console.log(resp);
+    return requestBody.json(resp.data)
   } catch (e) {
     console.error(e)
-    return res.status(500).send()
+    return {
+      statusCode: 500,
+    }
   }
 }
